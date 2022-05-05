e7be40cf30261008686f344cad87b994dc45bac2
Vertis-Z <48894365+Vertis-Z@users.noreply.github.com>
2022-05-05 01:16:59 -0400

    added return to top button in html, styling to button in css, and logic in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,21 @@ var top250 = function(){
         .then(response => console.log(response))
         .catch(err => console.error(err));
 };
+
+// return to top button logic
+mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
