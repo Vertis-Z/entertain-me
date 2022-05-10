241083cb2af0feabed58bbcf7c13e88391074acf
Michael Zelaya <Anhumimar@gmail.com>
2022-05-10 13:17:24 -0400

    added logic to ottMediaSearch

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,30 +6,6 @@ var searchInput = document.querySelector("#input-search");
 // var cardContainer = document.getElementById('card-section')
 
 // <-------------------IMDB-SCRAPPER API---------------------->
-// var top250Movies = function(){
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Host': 'imdb-scraper.p.rapidapi.com',
-//             'X-RapidAPI-Key': apikey1,
-//         }
-//     };
-
-//     fetch('https://imdb-scraper.p.rapidapi.com/top250', options)
-//         .then(response => response.json())
-//         .then(response => console.log(response))
-//         .then(response => {
-//           if (response) {
-//             response.json().then(function (data) {
-//               createTop250(data);
-//             })
-//           } else {
-//             $(".modal-append").empty();
-//             $(".modal-append").append("Error")
-//           }
-//         })
-//         .catch(err => console.error(err));
-// };
 
 var top250Movies = function () {
   fetch("https://imdb-scraper.p.rapidapi.com/top250", {
@@ -60,50 +36,65 @@ var top250Movies = function () {
     });
 };
 
-// var top250Shows = function(){
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Host': 'imdb-scraper.p.rapidapi.com',
-//       'X-RapidAPI-Key': apikey1,
+// var top250Movies = function () {
+//   fetch("https://imdb-scraper.p.rapidapi.com/toptv250", {
+//     "method": "GET",
+//     "headers": {
+//       "x-rapidapi-host": "imdb-scraper.p.rapidapi.com",
+//       "x-rapidapi-key": apikey1,
 //     }
-//   };
-
-//   fetch('https://imdb-scraper.p.rapidapi.com/toptv250', options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
+//   })
+//     .then(response => {
+//       if (response.ok) {
+//         response.json().then(function (data) {
+//           placeholder(data);
+//         })
+//       } else {
+//         $(".modal-append").empty();
+//         $(".modal-append").append("Error: " + response.statusText)
+//         let popup = new Foundation.Reveal($("#modal"));
+//         popup.open();
+//       }
+//     })
+//     .catch(err => {
+//       $(".modal-append").empty();
+//       $(".modal-append").append("Catch Error: Check console log")
+//       let popup = new Foundation.Reveal($("#modal"));
+//       popup.open();
+//       console.error(err);
+//     });
 // };
-// <---------------OTT DETAILS API----------------->
-// var mediaSearch = function (mediaquery) {
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Host': 'ott-details.p.rapidapi.com',
-//       'X-RapidAPI-Key': apikey2
-//     }
-//   };
 
-//   fetch('https://ott-details.p.rapidapi.com/search?title=' + mediaquery + '&page=1', options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-// };
+// <---------------------OTT DETAILS API----------------------->
 
-// var sourceFind = function(mediaFind){
-//   const options = {
-//     method: 'GET',
-//     headers: {
-//       'X-RapidAPI-Host': 'ott-details.p.rapidapi.com',
-//       'X-RapidAPI-Key': '7eb3dee3c7mshca977b3e70ebd78p1899f9jsnf27498159e6b'
-//     }
-//   };
-  
-//   fetch('https://ott-details.p.rapidapi.com/getPlatforms?region=US', options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-// }
+var ottMediaSearch = function () {
+  fetch('https://ott-details.p.rapidapi.com/gettitleDetails?imdbid=' + titleQuery, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "imdb-scraper.p.rapidapi.com",
+      "x-rapidapi-key": apikey2,
+    }
+  })
+    .then(response => {
+      if (response.ok) {
+        response.json().then(function (data) {
+          placeholder(data);
+        })
+      } else {
+        $(".modal-append").empty();
+        $(".modal-append").append("Error: " + response.statusText)
+        let popup = new Foundation.Reveal($("#modal"));
+        popup.open();
+      }
+    })
+    .catch(err => {
+      $(".modal-append").empty();
+      $(".modal-append").append("Catch Error: Check console log")
+      let popup = new Foundation.Reveal($("#modal"));
+      popup.open();
+      console.error(err);
+    });
+};
 
 //Get the button:
 mybutton = document.getElementById("topBtn");
@@ -165,7 +156,7 @@ var createTop250 = function (data) {
     textcenter.append(mediaDiv, synopisText, cardFooter, watchNow);
     mediaDiv.append(mediaContent);
     mediaContent.append(mediaTitle);
-    cardFooter.append(rating);
+    cardFooter.append(rating,addFavorite);
   };
 }
 
