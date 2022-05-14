d71675967bdc79fed9f93cb995ab9a0a20ad79b6
Michael Zelaya <Anhumimar@gmail.com>
2022-05-14 17:15:54 -0400

    added new imdb api and set up to make calls for title and rating

--- assets/js/searchresults.js
+++ assets/js/searchresults.js
@@ -15,7 +15,6 @@ var ottMediaSearch = function (searchValue) {
         .then(response => {
             if (response.ok) {
                 response.json().then(function (data) {
-                    // placeholder(data);
                     createResults(data);
                 })
             } else {
@@ -24,26 +23,37 @@ var ottMediaSearch = function (searchValue) {
             }
         })
         .catch(err => {
-            $(".modal-append").empty();
-            $(".modal-append").append("Catch Error: Check console log")
             console.log(err);
         });
 };
 
-// var ottMediaSearch = function (searchValue) {
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Host': 'ott-details.p.rapidapi.com',
-//             'X-RapidAPI-Key': apikey2
-//         }
-//     };
-    
-//     fetch('https://ott-details.p.rapidapi.com/search?title=' + searchValue + '&page=1', options)
-//         .then(response => response.json())
-//         .then(response => console.log(response))
-//         .catch(err => console.error(err));
-// }
+var imdbRating = function (searchValue) {
+    fetch('https://imdb-api.com/en/API/SearchTitle/k_1emmw9sw/' + searchValue, {
+        "method": "GET"
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else { text("Movie Not Found") }
+        }).then (data => {
+            fetch('https://imdb-api.com/en/API/Ratings/k_1emmw9sw/' + data.results[0].id, {
+                "method": "GET"
+            })
+                .then(response => {
+                    if (response.ok) {
+                        response.json().then(function (data) {
+                            console.log(data)
+                        })
+                    } else {
+                        $(".modal-append").empty();
+                        $(".modal-append").append("Error: " + response.statusText)
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        })
+}
 
 var createResults = function (data) {
     // empty previous data
@@ -54,6 +64,7 @@ var createResults = function (data) {
         let movieName = data.results[i].title;
         let movieImg = data.results[i].imageurl[0];
         let movieSynopsis = data.results[i].synopsis;
+        let mediaRating = data.results[i].imDb
 
         // create card for eatch [i]
         let outer_container = $("<div>").addClass("column is-4");
@@ -61,25 +72,25 @@ var createResults = function (data) {
         let cardImage = $("<div>").addClass("card-image");
         let imageis2by3 = $("<figure>").addClass("image is-2by3");
         // catch if no image is found
+        let mediaArt;
         if (data.results[i].imageurl[0] = null) {
-            let mediaArt = $("<img>").attr("src", "../images/image-not-available.png");
+            mediaArt = $("<img>").attr("src", "../images/image-not-available.png");
         } else {
-            let mediaArt = $("<img>").attr("src", movieImg);
+            mediaArt = $("<img>").attr("src", movieImg);
         };
-        // let mediaArt = $("<img>").attr("src", movieImg);
         let textcenter = $("<div>").addClass("card-content has-text-centered");
         let mediaDiv = $("<div>").addClass("media");
         let mediaContent = $("<div>").addClass("media-content");
         let mediaTitle = $("<p>").addClass("title is-4").text(movieName);
         // catch if no synopsis is found
+        let synopsisText
         if (data.results[i].synopsis = null) {
-            let synopsisText = $("<div>").addClass("content").text("Synopsis Not Found.");
+            synopsisText = $("<div>").addClass("content").text("Synopsis Not Found.");
         } else {
-            let synopsisText = $("<div>").addClass("content").text(movieSynopsis);
+            synopsisText = $("<div>").addClass("content").text(movieSynopsis);
         };
-        // let synopsisText = $("<div>").addClass("content").text(movieSynopsis);
         let cardFooter = $("<footer>").addClass("card-footer");
-        // let rating = $("<p>").addClass("card-footer-item").text(mediaRating);
+        let rating = $("<p>").addClass("card-footer-item").text(mediaRating);
         // let addFavorite = $("<a>").attr("href", "#").addClass("card-footer-item");
 
         // append cards
@@ -91,7 +102,7 @@ var createResults = function (data) {
         textcenter.append(mediaDiv, synopsisText, cardFooter, watchNow);
         mediaDiv.append(mediaContent);
         mediaContent.append(mediaTitle);
-        // cardFooter.append(rating, addFavorite);
+        cardFooter.append(rating, addFavorite);
     };
 }
 
@@ -110,7 +121,8 @@ var formSubmitHandler = function (event) {
 
     // need to replace 'spaces' with %20 for fetch request to work
     let searchMovieReplaceSpace = searchMovie.split(" ").join("%20");
-    ottMediaSearch(searchMovieReplaceSpace);
+    // ottMediaSearch(searchMovieReplaceSpace);
+    imdbRating(searchMovieReplaceSpace);
 
 }
 
@@ -137,7 +149,7 @@ var filmStorage = JSON.parse(window.localStorage.getItem("searchFilm")) || [];
 
 // need to replace 'spaces' with %20 for fetch request to work
 var filmStorageReplaceSpace = filmStorage.split(" ").join("%20");
-ottMediaSearch(filmStorageReplaceSpace);
+// ottMediaSearch(filmStorageReplaceSpace);
 
 /* ---------------------- Scroll to top button ---------------------- */
 
